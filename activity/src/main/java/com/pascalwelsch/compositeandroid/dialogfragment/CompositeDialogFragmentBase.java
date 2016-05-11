38a0b2ac486e44ce68f18d96ee6a7b3b35998f11
Pascal Welsch <pascal.welsch@gmail.com>
2016-05-11 17:06:45 +0200

    Support DialogFragment

---
+++ activity/src/main/java/com/pascalwelsch/compositeandroid/dialogfragment/CompositeDialogFragmentBase.java
@@ -0,0 +1,14 @@
+package com.pascalwelsch.compositeandroid.dialogfragment;
+
+import com.pascalwelsch.compositeandroid.core.Removable;
+
+import android.support.v4.app.DialogFragment;
+
+public class CompositeDialogFragmentBase extends DialogFragment {
+
+    protected DialogFragmentDelegate delegate = new DialogFragmentDelegate((CompositeDialogFragment) this);
+
+    public Removable addPlugin(final DialogFragmentPlugin plugin) {
+        return delegate.addPlugin(plugin);
+    }
+}
