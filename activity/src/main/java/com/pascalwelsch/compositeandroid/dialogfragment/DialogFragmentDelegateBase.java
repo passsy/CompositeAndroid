b92bb41318b8e0c388133936c2b27ade7615669e
Pascal Welsch <pascal.welsch@gmail.com>
2016-05-12 16:08:30 +0200

    Reformat DialogFragment code according to style

--- activity/src/main/java/com/pascalwelsch/compositeandroid/dialogfragment/DialogFragmentDelegateBase.java
+++ activity/src/main/java/com/pascalwelsch/compositeandroid/dialogfragment/DialogFragmentDelegateBase.java
@@ -4,7 +4,8 @@ import com.pascalwelsch.compositeandroid.core.AbstractDelegate;
 
 import android.support.v4.app.DialogFragment;
 
-public class DialogFragmentDelegateBase extends AbstractDelegate<DialogFragment, DialogFragmentPlugin> {
+public class DialogFragmentDelegateBase
+        extends AbstractDelegate<DialogFragment, DialogFragmentPlugin> {
 
     public DialogFragmentDelegateBase(final CompositeDialogFragment original) {
         super(original);
