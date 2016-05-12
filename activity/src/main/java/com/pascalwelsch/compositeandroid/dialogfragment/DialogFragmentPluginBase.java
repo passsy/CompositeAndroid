b39b5b1b568a4f245187caf954fcab6e3fb75d3f
Pascal Welsch <pascal.welsch@gmail.com>
2016-05-12 16:06:32 +0200

    Use correct delegate type for for CompositeDialogFragment plugins

--- activity/src/main/java/com/pascalwelsch/compositeandroid/dialogfragment/DialogFragmentPluginBase.java
+++ activity/src/main/java/com/pascalwelsch/compositeandroid/dialogfragment/DialogFragmentPluginBase.java
@@ -1,9 +1,9 @@
 package com.pascalwelsch.compositeandroid.dialogfragment;
 
 import com.pascalwelsch.compositeandroid.core.AbstractPlugin;
-import com.pascalwelsch.compositeandroid.fragment.FragmentDelegate;
 
-public class DialogFragmentPluginBase extends AbstractPlugin<CompositeDialogFragment, FragmentDelegate> {
+public class DialogFragmentPluginBase
+        extends AbstractPlugin<CompositeDialogFragment, DialogFragmentDelegate> {
 
     public CompositeDialogFragment getFragment() {
         return getOriginal();
